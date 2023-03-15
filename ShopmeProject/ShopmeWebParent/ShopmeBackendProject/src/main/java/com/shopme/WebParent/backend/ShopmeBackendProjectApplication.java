e2dc9b0fd9fb40ff4be0eb2e92ec4423b3492fea
BoredApe234 <harsh.malik@highwirepress.com>
2023-03-15 12:03:28 +0530

    first commit

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackendProject/src/main/java/com/shopme/WebParent/backend/ShopmeBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.WebParent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeBackendProjectApplication.class, args);
+	}
+
+}
---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEndProject/src/main/java/com/shopme/WebParent/Frontend/ShopmeFrontEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.WebParent.Frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeFrontEndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeFrontEndProjectApplication.class, args);
+	}
+
+}
